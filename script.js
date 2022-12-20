62dfa8a29d168d15cb39cc1864ef36244535e8cf
Matthew Troughton <mvt0408@gmail.com>
2022-12-19 22:15:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-alert("hello world");
 let emailElement = document.querySelector("#email");
 let messageElement = document.querySelector("#message");
+
 let submitButton = document.querySelector("#submit-button");
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
@@ -8,6 +8,6 @@ submitButton.addEventListener("click", function (e) {
   let emailValue = emailElement.value;
   let messageValue = messageElement.value;
 
-  console.log("Email: ", emailValue);
-  console.log("Message: ", messageValue);
+  console.log(emailValue);
+  console.log(messageValue);
 });
